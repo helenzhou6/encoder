701941029f16bd88a30cda6fc457d1d7d04fbd55
helenzhou6 <25727036+helenzhou6@users.noreply.github.com>
2025-06-23 17:46:50 +0100

    Update so eval_model needs to link with the SingleHeadAttentionModel new model

--- src/model/eval_model.py
+++ src/model/eval_model.py
@@ -1,48 +1,50 @@
 import os
 from torch import inference_mode, nn, load
 from torchvision import datasets
-from torchmetrics import classification, Accuracy
+import torchvision.transforms as transforms
+from torchmetrics import Accuracy
 from torch.utils.data import DataLoader
 from torchvision.transforms import ToTensor
 
 from utils import get_device
-from init_model import MNISTModel
-from run_model import input_shape, hidden_units, classification_num
+from init_model import SingleHeadAttentionModel
+from run_model import NUM_CATEGORIES, patch_image
 
 device = get_device()
 model_pth_path = 'model.pth'
 BATCH_SIZE = 32
 
+transform_image = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Lambda(patch_image)
+])
+
 test_data = datasets.MNIST(
     root="data",
     train=False,
     download=True,
-    transform=ToTensor()
+    transform=transform_image
 )
 test_dataloader = DataLoader(test_data,
     batch_size=BATCH_SIZE,
     shuffle=False
 )
 
-# -- Loads the model
 if os.path.exists(model_pth_path):
     print("model.pth exists, loading model state...")
-    model = MNISTModel(input_shape,
-        hidden_units,
-        classification_num
-    ).to(device)
+    model = SingleHeadAttentionModel(NUM_CATEGORIES, dim_k=49).to(device)
     model.load_state_dict(load('model.pth'))
 else:
     print("model.pth does not exist - please run run_model.py to create...")
 
 loss_fn = nn.CrossEntropyLoss()
-accuracy_fn = Accuracy(task = 'multiclass', num_classes=classification_num).to(device)
+accuracy_fn = Accuracy(task = 'multiclass', num_classes=NUM_CATEGORIES).to(device)
 
 loss, acc = 0, 0
 model.eval()
 with inference_mode():
     for X, y in test_dataloader:
-        y_pred = model(X)
+        (y_pred, _) = model(X)
         loss += loss_fn(y_pred, y)
         acc += accuracy_fn(y_pred.argmax(dim=1), y)
     loss /= len(test_dataloader)
@@ -51,6 +53,6 @@ with inference_mode():
     model_results_accuracy = (acc*100)
     
 if model_results_loss > 0.5 or model_results_accuracy < 90:
-    raise Exception(f"Machine learning model not usable - since model_loss was > 0.5 at {model_results_loss} and accuracy was < 90 at {model_results_accuracy}")
+    raise Exception(f"Machine learning model not usable - since model_loss was > 0.5 at {model_results_loss} and accuracy was < 90 at f{model_results_accuracy:.2f}%")
 else:
     print(f"Model passed evaluation: model loss {model_results_loss} & {model_results_accuracy:.2f}%")
--- src/model/run_model.py
+++ src/model/run_model.py
@@ -1,49 +1,54 @@
+from torch import nn
 from torchvision import datasets
 from torch.utils.data import DataLoader
-from torchvision.transforms import ToTensor
-from torch import nn, optim, inference_mode, save
-from torchmetrics import classification, Accuracy
-import wandb
-from init_model import MNISTModel
-from utils import init_wandb, get_device
+import torchvision.transforms as transforms
+from torchmetrics import Accuracy
+from torch import nn, optim, save
 
-device = get_device()
+from utils import get_device, init_wandb
+from init_model import SingleHeadAttentionModel
+
+PATCH_SIZE = 7
+EMBEDDING_DIM = PATCH_SIZE * PATCH_SIZE
 BATCH_SIZE = 32
-EPOCHS = 1
+EPOCHS = 5
 LEARNING_RATE = 0.1
+device = get_device()
 
-run = init_wandb()
+wandb_run = init_wandb()
 
-class_names = datasets.MNIST(
-    root="data",
-    train=True,
-    download=False,
-).classes
-classification_num=len(class_names)
+linear_proj = nn.Linear(PATCH_SIZE * PATCH_SIZE, EMBEDDING_DIM)
 
+def patch_image(image): # image = [1, 28, 28]
+    patches = image.unfold(1, PATCH_SIZE, PATCH_SIZE).unfold(2, PATCH_SIZE, PATCH_SIZE)
+    patches = patches.contiguous().view(-1, PATCH_SIZE * PATCH_SIZE) 
+    return linear_proj(patches)
+
+transform_image = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Lambda(patch_image)
+])
+    
 train_data = datasets.MNIST(
     root="data",
     train=True,
     download=True,
-    transform=ToTensor(),
+    transform=transform_image,
     target_transform=None
 )
+
 train_dataloader = DataLoader(train_data,
     batch_size=BATCH_SIZE,
     shuffle=True
 )
 
-input_shape = 784 
-embedding_dim = 30
+NUM_CATEGORIES = len(train_data.classes)
 
-model = MNISTModel(input_shape, embedding_dim, classification_num, 
-        ).to(device)
+model = SingleHeadAttentionModel(NUM_CATEGORIES, dim_k=49).to(device)
 
-accuracy_fn = Accuracy(task = 'multiclass', num_classes=classification_num).to(device)
+accuracy_fn = Accuracy(task = 'multiclass', num_classes=NUM_CATEGORIES).to(device)
 loss_fn = nn.CrossEntropyLoss()
 model_optimizer = optim.SGD(params=model.parameters(), lr=LEARNING_RATE)
-
-# -- TRAINING --
 
 def train_model():
     for epoch in range(EPOCHS):
@@ -51,9 +56,9 @@ def train_model():
         train_loss, train_acc = 0, 0
         # y = classification
         for _, (batch_images, actual_y) in enumerate(train_dataloader):
-            # Image is torch.Size([32, 1, 28, 28]) = batchsize, grayscale (1 channel) of 28px x 28px & is a torch.float32
+            # batch_images is [32, 16, 64]
             model.train()
-            y_pred = model(batch_images)
+            (y_pred, _) = model(batch_images)
             loss = loss_fn(y_pred, actual_y)
             train_loss += loss 
             train_acc += accuracy_fn(y_pred.argmax(dim=1), actual_y)
@@ -64,10 +69,10 @@ def train_model():
         train_loss /= len(train_dataloader)
         train_acc /= len(train_dataloader)
         print(f"Train loss: {train_loss:.5f} | Train accuracy: {(train_acc*100):.2f}%")
-        run.log({"acc": train_acc*100, "loss": train_loss})
+        wandb_run.log({"acc": train_acc*100, "loss": train_loss})
 
     save(model.state_dict(), 'model.pth')
 
 # RUN below to train it
 # train_model()
-run.finish()
+wandb_run.finish()
