4c506e8e18d40e07553e1633a818f55878458b23
helenzhou6 <25727036+helenzhou6@users.noreply.github.com>
2025-06-24 16:36:36 +0100

    Change the code to the more simplified use of qkv_proj:

--- src/model/init_model.py
+++ src/model/init_model.py
@@ -5,39 +5,30 @@ import torch
 class SingleHeadAttentionModel(nn.Module):
     def __init__(self, output_shape, num_patches, dim_input, dim_k):
         super().__init__()
-        self.query_proj = nn.Linear(dim_input, dim_k, bias=False)
-        self.key_proj = nn.Linear(dim_input, dim_k, bias=False)
-        self.val_proj = nn.Linear(dim_input, dim_k, bias=False)
         self.dim_k = dim_k
         self.num_patches = num_patches
+        # Combined projection for Q, K, V
+        self.qkv_proj = nn.Linear(dim_input, dim_k * 3, bias=False)
 
         self.position_patch_embed = nn.Parameter(torch.zeros(1, num_patches, dim_k))
         # TODO: REMOVE BELOW when we link it will the decoder
         self.classifier = nn.Linear(dim_k, output_shape)
 
     def forward(self, x):
-        # x: (batch_size, num_patches, dim_input)
-        Q = self.query_proj(x)  # (batch_size, num_patches, dim_k)
-        K = self.key_proj(x)    # (batch_size, num_patches, dim_k)
-        V = self.val_proj(x)    # (batch_size, num_patches, dim_k)
+        # Project to QKV and split into 3 tensors along last dimension
+        # x is (batch_size, num_patches, dim_input)
+        qkv = self.qkv_proj(x)  # shape: (batch_size, num_patches, dim_k * 3)
+        Q, K, V = qkv.split(self.dim_k, dim=-1)  # each is (batch_size, num_patches, dim_k)
 
-        # Add positional embedding after projection
         Q = Q + self.position_patch_embed # (batch_size, num_patches, dim_k)
         K = K + self.position_patch_embed
         V = V + self.position_patch_embed
 
-        
-        dot_product_keys_queries = torch.matmul(Q, K.transpose(-2, -1))
-        sq_root_dim = (self.dim_k ** 0.5)
-        attention = dot_product_keys_queries / sq_root_dim
-
-        normalised_attention = torch.softmax(attention, dim=-1)
-
-        hidden_v_representation = torch.matmul(normalised_attention, V)
+        attention_out = nn.functional.scaled_dot_product_attention(Q, K, V, is_causal=False) # shape: (batch_size, num_patches, dim_k)
         # TODO: add output x W = H as last step
 
         # TODO: REMOVE THE BELOW when we link it will the decoder   
-        # Aggregate over patches: e.g., take mean or sum over num_patches dimension
-        pooled_output = hidden_v_representation.mean(dim=1)  # (batch_size, dim_k)
-        logits = self.classifier(pooled_output) # (batch_size, output_shape)
-        return logits, normalised_attention
+        # Aggregate over patches: e.g.(mean pooling) take mean or sum over num_patches dimension
+        pooled_output = attention_out.mean(dim=1)  # (batch_size, dim_k)
+        logits = self.classifier(pooled_output)  # (batch_size, output_shape)
+        return logits, attention_out
