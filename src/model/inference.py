ad8c5d4ed65584f4a7f349b59e89ea6c559addff
helenzhou6 <25727036+helenzhou6@users.noreply.github.com>
2025-06-25 15:03:16 +0100

    Remove duplicate code (grayscaling twice)

--- src/model/inference.py
+++ src/model/inference.py
@@ -1,5 +1,5 @@
-from torch import nn, no_grad, max, float32, Size
-from torchvision.transforms import Compose, ToTensor, Resize, Lambda
+from torch import no_grad
+from torchvision.transforms import Compose, ToTensor, Resize, Lambda, Normalize, Grayscale
 from PIL import Image
 from src.model.run_model import patch_image
 from src.model.utils import get_device
@@ -13,16 +13,16 @@ def _predict_digit_using_model(tensor_digit, model, device):
         return result
 
 _transform_to_tensor = Compose([
+    Grayscale(num_output_channels=1),
     Resize((28, 28)),
     ToTensor(),
-    Lambda(patch_image)
+    Lambda(patch_image),
 ])
 
 def _process_image(uint8_img):
     #  Process image from 280 pixel x 280 pixel, 4 colour channels (3 RGB + 1 alpha) uint8 to tensor (that is 28 x 28 and 1 greyscale channel)
     drawn_image = Image.fromarray(uint8_img)
-    drawn_image_grey = drawn_image.convert("L") # convert to grayscale
-    tensor_digit = _transform_to_tensor(drawn_image_grey)
+    tensor_digit = _transform_to_tensor(drawn_image)
     return tensor_digit.unsqueeze(0)
 
 def predict_digit(uint8_img, model):
